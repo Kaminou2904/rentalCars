a32c13bcc3cb6807296cc36dbc94398e8bd89f57
Vikas Kanoo <vikaskanoozzz@gamil.com>
2023-08-21 12:40:26 +0530

    detail page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Detail from './Pages/Detail/Detail';
 
 function App() {
   return (
     <div>
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Detail' element={<Detail/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
